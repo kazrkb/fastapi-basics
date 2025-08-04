b73d612c9768141d94dcbb365dae3027f4e678b4
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2025-08-04 22:00:36 +0600

    get request

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "Welcome to learning fast api"}
+
+@app.get('/posts')
+def get_post():
+    return {
+        'data': 'this is your post'
+    }
