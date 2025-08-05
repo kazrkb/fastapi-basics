d8d5f2cddf35bbbf7a2e491a47192f6c33f789eb
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2025-08-05 12:50:22 +0600

    created schema for sending data from frontend

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # pip install "fastapi[standard]" "uvicorn[standard]" sqlalchemy python-dotenv pydantic
 
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    publish: bool = True
 
 @app.get("/")
 def read_root():
@@ -16,9 +22,12 @@ def get_post():
     }
     
 @app.post('/createpost')
-def create_post(payLoad: dict = Body(...)):
-    print(payLoad)
+# def create_post(payLoad: dict = Body(...)):
+def create_post(new_post: Post):
+    # print(payLoad)
+    print(new_post.publish)
     return {
-        'message': 'successfully created post',
-        'newpost': f"title {payLoad['title']} content {payLoad['content']}"
+        # 'message': 'successfully created post',
+        # 'newpost': f"title {payLoad['title']} content {payLoad['content']}"
+        "data": "new post successful"
     }
