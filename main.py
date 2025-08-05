4eaa5bc6121f257a9a6a43b8c3c39339d69a124f
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2025-08-05 13:09:01 +0600

    send optional data to check the pydantic basemodel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # pip install "fastapi[standard]" "uvicorn[standard]" sqlalchemy python-dotenv pydantic
 
+from typing import Optional
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
 
@@ -10,6 +11,7 @@ class Post(BaseModel):
     title: str
     content: str
     publish: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def read_root():
@@ -26,6 +28,7 @@ def get_post():
 def create_post(new_post: Post):
     # print(payLoad)
     print(new_post.publish)
+    print(new_post.rating)
     return {
         # 'message': 'successfully created post',
         # 'newpost': f"title {payLoad['title']} content {payLoad['content']}"
